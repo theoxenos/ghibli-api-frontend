a1c3688d87e6823350da99a1749951d5b47dd55b
Theoxenos <theoxenos@pm.me>
2026-04-15 14:05:20 +0200

    Fix links in topnavmenu component

--- src/features/topnavmenu/components/TopNavMenu.tsx
+++ src/features/topnavmenu/components/TopNavMenu.tsx
@@ -2,7 +2,7 @@ export const TopNavMenu = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Studio Ghibli Films</a>
+                <a className="navbar-brand" href="/">Studio Ghibli Films</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -11,7 +11,7 @@ export const TopNavMenu = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
                         </li>
                     </ul>
                 </div>
